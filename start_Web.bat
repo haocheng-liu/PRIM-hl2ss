862e1bd413ca937f3675841dc1a9ca305e074ff3
Haocheng <haocheng@outlook.fr>
2025-11-30 22:35:47 +0100

    fix datapath args in web main

--- start_Web.bat
+++ start_Web.bat
@@ -3,9 +3,11 @@ setlocal
 
 set "DEFAULT_HOST=192.168.1.36"
 set "PYTHON_EXE=%~dp0python-3.11.0\python.exe"
-set "SCRIPT_DIR=%~dp0prim_viewer"
+set "SCRIPT_DIR=%~dp0prim_web"
 set "SCRIPT_NAME=main.py"
 set "DEFAULT_ROOM=None"
+
+set "DATASET_DIR=%~dp0hl2ss-lk\viewer\dataset"
 
 echo.
 echo Starting...
@@ -15,7 +17,7 @@ echo.
 
 cd /d "%SCRIPT_DIR%"
 
-"%PYTHON_EXE%" "%SCRIPT_NAME%"
+"%PYTHON_EXE%" "%SCRIPT_NAME%" --dataset "%DATASET_DIR%"
 
 echo.
 echo DONE.
